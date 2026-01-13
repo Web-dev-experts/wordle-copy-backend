c14a4549f228a8159a47e82510af20c9030a2c84
Web-dev-experts <itworksonmymachinedev@gmail.com>
2026-01-13 06:48:24 +0100

    Fix modul validattion

--- utils/createNewGame.cjs
+++ utils/createNewGame.cjs
@@ -1,11 +1,11 @@
-const Game = require("../models/gameModel.cjs");
+const Game = require('../models/gameModel.cjs');
 
 const pickRandomWord = async function () {
   try {
     const res = await fetch(
-      "https://random-word-api.herokuapp.com/word?length=5"
+      'https://random-word-api.herokuapp.com/word?length=5'
     );
-    if (!res.ok) throw new Error("Could not fetch");
+    if (!res.ok) throw new Error('Could not fetch');
     const fetched = await res.json();
     return fetched[0];
   } catch (error) {
@@ -18,11 +18,11 @@ exports.createDailyGame = async function () {
   // Optional: remove expired games
   await Game.deleteMany({ expiresAt: { $lt: new Date() } });
 
-  const word = pickRandomWord(); // your logic
+  const word = await pickRandomWord(); // your logic
 
   await Game.create({
     word,
   });
 
-  console.log("✅ New daily game created");
+  console.log('✅ New daily game created');
 };
